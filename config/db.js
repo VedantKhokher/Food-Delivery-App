4ff845447ee6d23c3c8bb0fc26a6d3a8dc6a1c08
VedantKhokher <91693392+VedantKhokher@users.noreply.github.com>
2024-08-16 19:03:42 +0530

    Delete config directory

--- config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB=async ()=>{
-    await mongoose.connect('mongodb+srv://keshavgoyal1107:rkn5W7TRaYtAlye3@cluster0.rfssd71.mongodb.net/foodApp').then(()=>console.log("DB connected"));
-
-}
